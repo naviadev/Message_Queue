baf8b65a44d55588c471958a66b202da21d88929
naviadev <qo2oq1020@gmail.com>
2024-06-29 15:42:46 +0900

    FEAT | EventBroker.ts : Members init
    type : {string : queue}
    description : 이벤트 타입과 해당하는 큐를 갖는다. 만약 A라는 이벤트 타입이 존재한다면 A라는 요청들을 담는 Queue를 생성
    
    type : {string : Subscriber[]}
    description : 미리 정의해둔 interface인 Subscriber 형태의 객체를 배열로 받아온다 .
    이벤트 타입에 해당하는 로직들을 forEach를 통해 실행하여 각각의 요청들을 순차적으로 처리하기 위한 멤버변수

---
+++ server/modules/EventBroker.ts
@@ -0,0 +1,16 @@
+import { Subscriber } from "../interface/Subscriber";
+import { MessageQueue } from "./MessageQueue";
+
+/** 
+ * @naviadev 
+ * EventBroker Class
+ * ? 멤버변수
+ * 
+ * @queues : 이벤트 타입을 key로 가지며, 생성된 key만큼의 Queue를 할당한다. 
+ * @subscribers : 이벤트 타입 별로 구독자(model) 리스트를 저장한다.
+ */
+class EventBroker {
+  private queues: { [key: string]: MessageQueue } = {};
+  private subscribers: { [key: string]: Subscriber[] } = {};
+
+}
