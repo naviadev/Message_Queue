e194e6c4027c3b0cd137e67273e90a66f57702e2
naviadev <qo2oq1020@gmail.com>
2024-06-29 18:50:47 +0900

    FIX | updatePort.ts : 잘못된 env파일 경로 수정

--- server/utility/updatePort.ts
+++ server/utility/updatePort.ts
@@ -2,13 +2,16 @@ import path from 'path';
 import dotenv from 'dotenv';
 import fs from 'fs';
 const rootDir = process.cwd();
-dotenv.config({ path: '../../../.env' });
+dotenv.config({ path: `${rootDir}/.env` });
+console.log(`${rootDir}.env`)
 const getRandomPort = (min = process.env.MIN_PORT, max = process.env.MAX_PORT): number => {
   return Math.floor(Math.random() * (Number(max) - Number(min) + 1) + Number(min));
 }
+console.log(process.env.MIN_PORT)
 const port: number = getRandomPort();
 const upDateEnvPort = (RandPort: number = port): void => {
-  const envFilePath = path.resolve(rootDir, '../../../.env');
+  const envFilePath = path.resolve(rootDir, '.env');
+  console.log(envFilePath)
   const envConfig = dotenv.parse(fs.readFileSync(envFilePath));
   envConfig.PORT = RandPort.toString();
   const newEnvContent: string = Object.keys(envConfig)
