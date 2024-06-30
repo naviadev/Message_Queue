e1616bc21c1066c28ba80f03594d565c6c2e4fbf
naviadev <qo2oq1020@gmail.com>
2024-06-30 16:26:41 +0900

    FEAT | app.ts : eventBroker에 서비스 추가, 엔드포인트 추가, 랜덤포트 적용

--- server/app.ts
+++ server/app.ts
@@ -11,6 +11,20 @@ dotenv.config({ path: `${rootDir}/.env` });
 const app = express();
 const eventBroker = new EventBroker();
 const orderService = new OrderService();
-
 //미들웨어 설정
 app.use(express.json());
+// orderService 모듈을 등록.
+eventBroker.addSubscribe('order', orderService);
+
+app.post('/order', async (req: Request, res: Response) => {
+  const { eventType, message } = req.body;
+  if (!eventType || !message) {
+    return res.status(400).send('잘못된 요청입니다.');
+  }
+  eventBroker.publish(eventType, message);
+  res.send('Event published')
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on http://localhost:${process.env.PORT}`);
+});
